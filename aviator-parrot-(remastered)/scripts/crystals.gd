2fa1944334457bde64dc41a108601e6df54ef28a
Kou-ding <fotispapadak@gmail.com>
2026-02-05 20:38:14 +0200

    - faster crystals - bouncy bird

--- aviator-parrot-(remastered)/scripts/crystals.gd
+++ aviator-parrot-(remastered)/scripts/crystals.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var crystals_speed = 450
+var crystals_speed = 700
 
 signal passed
 
--- aviator-parrot-(remastered)/scripts/parrot.gd
+++ aviator-parrot-(remastered)/scripts/parrot.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-const JUMP_VELOCITY = -500.0
+const JUMP_VELOCITY = -1300.0
 var jump_button = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta * 1.5
+		velocity += 3 * get_gravity() * delta
 		if velocity.y>0:
 			#rotation = 0.5
 			$AnimatedSprite2D.frame=0

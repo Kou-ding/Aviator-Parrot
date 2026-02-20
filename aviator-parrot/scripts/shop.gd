71c328ce26a43eadda06f5234c0c45c7cb2c385e
Kou-ding <fotispapadak@gmail.com>
2026-02-20 03:22:38 +0200

    feat(shop): Implement shop functionality with player selection and currency management
    - Added shop modes for buying, selecting, and viewing players.
    - Implemented functions to configure player details and manage owned players.
    - Introduced currency handling for purchasing players.
    - Added visual feedback for selected and owned players.
    - Created utility functions for saving and loading player data.
    - Added lock assets for unavailable players.
    - Updated UI elements to reflect changes in player ownership and selection.

--- aviator-parrot/scripts/shop.gd
+++ aviator-parrot/scripts/shop.gd
@@ -1,11 +1,132 @@
 extends Control
 
+var active_player
+var shop_mode
+var item_price
+enum ShopMode {buying,selecting,viewing}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$DetailsPanel.hide()
+	render_locks()
+	#DirAccess.remove_absolute("user://owned_players.save")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func render_locks():
+	if Utils.player_is_owned("parrot"):
+		$ScrollContainer/GridContainer/parrot/lock.hide()
+		$ScrollContainer/GridContainer/parrot/TextureRect.hide()
+	if Utils.player_is_owned("slime"):
+		$ScrollContainer/GridContainer/slime/lock.hide()
+		$ScrollContainer/GridContainer/slime/TextureRect.hide()
+
+func configure_details(player_tag,player_price):
+	# Allow buying if not owned and selecting if not selected
+	if Utils.player_is_owned(player_tag) and Utils.player_is_selected(player_tag):
+		$DetailsPanel/BuySelect.text = "SELECTED"
+		$DetailsPanel/BuySelect/CoinSymbol.hide()
+		shop_mode = ShopMode.viewing
+	elif Utils.player_is_owned(player_tag) and not Utils.player_is_selected(player_tag):
+		$DetailsPanel/BuySelect.text = "SELECT"
+		$DetailsPanel/BuySelect/CoinSymbol.hide()
+		shop_mode = ShopMode.selecting
+	else:
+		$DetailsPanel/BuySelect.text = "BUY - " + str(player_price)
+		item_price = player_price
+		shop_mode = ShopMode.buying
+
+func darken():
+	$Background.modulate = Color(0.5, 0.5, 0.5)
+	$Control.modulate = Color(0.5, 0.5, 0.5)
+	$ScrollContainer.modulate = Color(0.5, 0.5, 0.5)
+	$ScrollContainer2.modulate = Color(0.5, 0.5, 0.5)
+	$PlayersLabel.modulate = Color(0.5, 0.5, 0.5)
+	$BackgroundsLabel.modulate = Color(0.5, 0.5, 0.5)
+	$Control/MarginContainer/HBoxContainer/Back.disabled = true
+
+func undarken():
+	$Background.modulate = Color(1, 1, 1)
+	$Control.modulate = Color(1, 1, 1)
+	$ScrollContainer.modulate = Color(1, 1, 1)
+	$ScrollContainer2.modulate = Color(1, 1, 1)
+	$PlayersLabel.modulate = Color(1, 1, 1)
+	$BackgroundsLabel.modulate = Color(1, 1, 1)
+	$Control/MarginContainer/HBoxContainer/Back.disabled = false
+	
+func _on_back_pressed() -> void:
+	$Button_Sound.play()
+	await $Button_Sound.finished
+	get_tree().change_scene_to_file("res://scenes/start_screen.tscn")
+
+# All Available upgrades 
+func _on_parrot_pressed() -> void:
+	var player_tag = "parrot"
+	active_player=player_tag
+	
+	# Name
+	$DetailsPanel/NameMargin/ItemName.text = player_tag
+	# Icon
+	var player_icon = $DetailsPanel/MarginContainer/ItemIcon
+	var atlas_texture = AtlasTexture.new()
+	atlas_texture.atlas = load("res://assets/mc.png")
+	atlas_texture.region = Rect2(0, 0, 456.0, 424.0)
+	player_icon.texture = atlas_texture
+	
+	# Buy, select, selected bar
+	configure_details(player_tag,1)
+	$DetailsPanel.show()
+	darken()
+
+
+func _on_slime_pressed() -> void:
+	var player_tag = "slime"
+	active_player=player_tag
+	
+	# Name
+	$DetailsPanel/NameMargin/ItemName.text = player_tag
+	# Icon
+	var player_icon = $DetailsPanel/MarginContainer/ItemIcon
+	var atlas_texture = AtlasTexture.new()
+	atlas_texture.atlas = load("res://assets/slime.png")
+	atlas_texture.region = Rect2(720,160, 464.0, 376.0)
+	player_icon.texture = atlas_texture
+	
+	# Buy, select, selected bar
+	configure_details(player_tag,1)
+	$DetailsPanel.show()
+	darken()
+
+
+
+func _on_moon_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_space_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_buy_select_pressed() -> void:
+	if shop_mode == ShopMode.buying:
+		var current_currency = Utils.load_currency()
+		Utils.save_currency(current_currency - item_price)
+		Utils.buy_Player(active_player)
+		Utils.set_Player(active_player)
+		$DetailsPanel/BuySelect.text = "SELECT"
+		shop_mode = ShopMode.selecting
+		return
+	if shop_mode == ShopMode.selecting:
+		Utils.set_Player(active_player)
+		$DetailsPanel/BuySelect.text = "SELECTED"
+		return
+
+func _on_exit_details_pressed() -> void:
+	$Button_Sound.play()
+	await $Button_Sound.finished
+	$DetailsPanel.hide()
+	render_locks()
+	undarken()
--- aviator-parrot/scripts/utils.gd
+++ aviator-parrot/scripts/utils.gd
@@ -6,9 +6,11 @@ const LEADERBOARD_SAVE_PATH := "user://leaderboard.save"
 const CURRENCY_SAVE_PATH := "user://currency.save"
 const SETTINGS_SAVE_PATH := "user://settings.save"
 const PLAYER_SAVE_PATH := "user://player.save"
-const BG_SAVE_PATH : = "user://bg.save"
+const BG_SAVE_PATH := "user://bg.save"
+const OWNED_PLAYERS_SAVE_PATH := "user://owned_players.save"
 const MAX_SCORES := 8
 
+# Leaderboard data
 static func load_leaderboard() -> Array:
 	var scores: Array = []
 	
@@ -25,20 +27,20 @@ static func save_leaderboard(scores: Array) -> void:
 
 static func update_leaderboard(score) -> void:
 	var scores = load_leaderboard()
-	
 	scores.append(score)
 	scores.sort()
 	scores.reverse() # highest first
 	
 	if scores.size() > MAX_SCORES:
 		scores = scores.slice(0, MAX_SCORES)
-	
 	save_leaderboard(scores)
+
+# Currency data
 static func save_currency(score) -> void:
 	var file = FileAccess.open(CURRENCY_SAVE_PATH,FileAccess.WRITE)
 	file.store_var(score)
 	file.close()
-	
+
 static func load_currency() -> int:
 	var total_coins:int
 	if FileAccess.file_exists(CURRENCY_SAVE_PATH):
@@ -46,15 +48,15 @@ static func load_currency() -> int:
 		total_coins = file.get_var()
 		file.close()
 	return total_coins
-
+	
 static func update_currency(score) -> void:
 	var total_coins = load_currency()
 	total_coins += score
 	save_currency(total_coins)
 
+# Render 
 static func format_numbered_list(array: Array) -> String:
 	var result: String = ""
-
 	for i in range(array.size()):
 		var number = i + 1  # Start counting from 1
 		var item = str(array[i])  # Convert to string in case items aren't strings
@@ -64,21 +66,19 @@ static func format_numbered_list(array: Array) -> String:
 		if i < array.size() - 1:
 			result += "\n"
 	return result
-	
+
 static func format_numbered_list_shortVer(array: Array) -> String:
 	var result: String = ""
-
 	for i in range(3):
 		var number = i + 1  # Start counting from 1
 		var item = str(array[i])  # Convert to string in case items aren't strings
 		result += str(number) + ". " + item + " points"
-
 		# Add newline except for last item
 		if i < array.size() - 1:
 			result += "\n"
-	
 	return result
-	
+
+# Settings permanence throughout game
 static func save_settings_before_quitting():
 	var current_music_volume = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music"))
 	var current_sfx_volume = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("SFX"))
@@ -90,7 +90,7 @@ static func load_settings_after_startup():
 	var previous_sfx_volume = previous_volume[1]
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"),previous_music_volume)
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"),previous_sfx_volume)
-	
+
 static func save_settings(music_volume,sfx_volume) -> void:
 	var file = FileAccess.open(SETTINGS_SAVE_PATH, FileAccess.WRITE)
 	if file:
@@ -104,8 +104,10 @@ static func load_settings() -> Array:
 	if file:
 		var data = file.get_var()
 		return data
+	file.close()
 	return [0,0]
 
+# Player
 static func set_Player(player_tag):
 	var file = FileAccess.open(PLAYER_SAVE_PATH, FileAccess.WRITE)
 	if file:
@@ -117,17 +119,74 @@ static func load_Player():
 		return "parrot"
 	var file = FileAccess.open(PLAYER_SAVE_PATH, FileAccess.READ)
 	var data = file.get_var()
+	file.close()
 	return data
-	
+
+static func owned_Players():
+	# If trying to access owned players for the first time add the default character
+	if not FileAccess.file_exists(OWNED_PLAYERS_SAVE_PATH):
+		var file_w = FileAccess.open(OWNED_PLAYERS_SAVE_PATH, FileAccess.WRITE)
+		if file_w:
+			var default_player = "parrot"
+			file_w.store_line(default_player)
+			file_w.close()
+	# Return owned players
+	var file_r = FileAccess.open(OWNED_PLAYERS_SAVE_PATH, FileAccess.READ)
+	var owned_players = []
+	var player = ""
+	while file_r.get_position()<file_r.get_length():
+		player = file_r.get_line()
+		if player!="":
+			owned_players.append(player)
+			print("yes append")
+			print("player:"+player)
+			print("owned players:")
+			print(owned_players)
+		
+	return owned_players
+
+
+static func player_is_owned(player_tag):
+	var players_list = owned_Players()
+	print("Player list")
+	print(players_list)
+	for player in players_list:
+		print("player_tag:" + player_tag)
+		print("player:"+player)
+		if player == player_tag:
+			return true
+	return false
+
+static func player_is_selected(player_tag):
+	var current_player = load_Player()
+	if player_tag == current_player:
+		return true
+	return false
+
+static func buy_Player(player_tag):
+	var owned_players = owned_Players()
+	owned_players = [owned_players]
+	# Add the new player if not already owned
+	if player_tag not in owned_players:
+		owned_players.append(player_tag)
+
+	var file = FileAccess.open(OWNED_PLAYERS_SAVE_PATH, FileAccess.WRITE)
+	if file:
+		file.store_var(owned_players)
+	file.close()
+
+# Bg
 static func set_Bg(bg_tag):
 	var file = FileAccess.open(BG_SAVE_PATH, FileAccess.WRITE)
 	if file:
 		var data = bg_tag
 		file.store_var(data)
+	file.close()
 
 static func load_Bg():
 	if not FileAccess.file_exists(BG_SAVE_PATH):
 		return "stary night"
 	var file = FileAccess.open(BG_SAVE_PATH, FileAccess.READ)
 	var data = file.get_var()
+	file.close()
 	return data
